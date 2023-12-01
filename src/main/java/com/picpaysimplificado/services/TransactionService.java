a512d2d1b96fd776f192360a0279dfd795cb6e9e
Matheus Dias <hmatheus031@gmail.com>
2023-11-30 21:07:03 -0300

    feat: creating TransactionService

---
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -0,0 +1,54 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.transaction.Transaction;
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.TransactionDTO;
+import com.picpaysimplificado.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class TransactionService {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionRepository repository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void createTransaction(TransactionDTO transaction) throws Exception {
+        User sender = this.userService.findUserById(transaction.senderId());
+        User receiver = this.userService.findUserById(transaction.receiverId());
+
+        userService.validateTransaction(sender, transaction.value());
+
+        boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
+        if(!isAuthorized) {
+            throw new Exception("Transação não autorizada");
+        }
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setAmount(transaction.getAmount());
+        newTransaction.setSender(sender);
+        newTransaction.setReceiver(receiver);
+        newTransaction.setTimeStamp(LocalDateTime.now());
+    }
+
+    public boolean authorizeTransaction(User sender, BigDecimal value) {
+        ResponseEntity<Map> authorizeResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+
+        if(authorizeResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) authorizeResponse.getBody().get("message");
+            return "Autorizado".equalsIgnoreCase(message);
+        } else return false;
+    }
+}
